ac96151c8a93e24498ff79315cac4e2b65233e2e
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-09-29 23:14:12 +0700

    Add files system based routing

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from './app.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
---
+++ src/pages/[...404].vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>404</h1>
+        <router-link to="/">Go back</router-link>
+    </div>
+</template>
