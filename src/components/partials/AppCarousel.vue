80d2448c935e6c445ff23fa67dd556e8813ce779
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-10-02 14:11:45 +0700

    wip

---
+++ src/components/partials/AppCarousel.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import 'vue3-carousel/carousel.css'
+import { Carousel, Slide } from 'vue3-carousel';
+import { ref } from 'vue';
+
+const props = defineProps<{
+    data: string[]
+}>();
+
+const height = ref(window.innerHeight);
+</script>
+
+<template>
+    <Carousel class="overflow-hidden" :transition="600" :gap="16" :mouse-wheel="true" :items-to-show="1"
+        :items-to-scroll="1" dir="ttb" :height="height">
+        <Slide v-for="(item, index) in props.data" :key="index">
+            <img :src="item" alt="" class="h-full cursor-pointer w-fit object-contain rounded-2xl">
+        </Slide>
+    </Carousel>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import 'vue3-carousel/carousel.css'
-import { Carousel, Slide } from 'vue3-carousel';
-import { computed, ref } from 'vue';
-
 definePage({
     'meta': {
         title: 'Home'
@@ -18,26 +14,10 @@ const images = [
     "https://picsum.photos/id/1020/800/400",
     "https://images.unsplash.com/photo-1519699047748-de8e457a634e?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ]
-
-// const height = ref(800);
-
-// const caculateHeight = (value) => {
-//     let img = new Image();
-//     img.src = images[value.currentSlideIndex];
-//     console.log(img.width, img.height);
-// }
 </script>
 
 <template>
     <div class="flex items-center justify-center m-5">
-        <Carousel @slide-start="caculateHeight" class="overflow-hidden" :transition="600" :gap="16" :mouse-wheel="true"
-            :items-to-show="1" :items-to-scroll="1" :height="800" dir="ttb" v-slot="{ currentSlide }">
-            <Slide v-for="(img, index) in images" :key="index" class="transition-all duration-600 ease-in-out" :class="{
-                'opacity-100 scale-100': index == currentSlide,
-                'opacity-0 scale-95': currentSlide < index < currentSlide
-            }">
-                <img :src="img" alt="" class="h-full cursor-pointer w-fit object-contain rounded-2xl">
-            </Slide>
-        </Carousel>
+        <AppCarousel :data="images" />
     </div>
 </template>
