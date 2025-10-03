1fd29891a6827073e5c80fb8d60cfc3b8cd55a9b
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-10-04 00:26:48 +0700

    Add progress bar to vue router

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import 'nprogress/nprogress.css'
+import './assets/nprogress.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
+import NProgress from 'nprogress'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 })
+
+if (typeof document !== 'undefined') {
+    NProgress.configure({
+        showSpinner: false,
+    })
+    let timeout: ReturnType<typeof setTimeout> | undefined
+    router.beforeEach((to) => {
+        const delay = Array.isArray(to.meta.loaders) && to.meta.loaders.length > 0 ? 200 : 0
+        timeout = setTimeout(() => {
+            if (router.currentRoute.value !== to) {
+                NProgress.start()
+            }
+        }, delay)
+    })
+
+    router.afterEach(() => {
+        clearTimeout(timeout)
+        NProgress.done(false)
+    })
+
+    router.onError(() => {
+        clearTimeout(timeout)
+        NProgress.done(false)
+    })
+}
 
 if (import.meta.hot) {
     handleHotUpdate(router)
